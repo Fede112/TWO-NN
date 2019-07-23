964a2c3c1ee28098b99247b1628a1aa443dccf24
Fede <fedebarone@gmail.com>
2019-07-23 11:30:57 +0200

    working version of python code

--- TWO-NN_userfriendly.cc
+++ TWO-NN_userfriendly.cc
@@ -199,6 +199,8 @@ void compute_d(vector<double>& D1, vector<double>& D2, double& dim, int N, doubl
 		N_retained=Ncut;
 	}
 
+	cout << "Ncut: " << Ncut << endl;
+
 	for(int i=0; i<Ncut; i++)
 	{
 		sumX += XX[i];
@@ -399,11 +401,20 @@ int main(int argc, char* argv[])
 		IND.push_back(i);
 	}
 	std::random_shuffle(IND.begin(), IND.end());
+	
+	ofstream file_ind("index.dat");
+
+	for(int i=0; i<N; i++)
+	{
+		file_ind << IND.at(i) << endl;
+	}
+
+	file_ind.close();
 
 	// BLOCK ANALYSIS
 
-	int NBOX[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,20};
-	//int NBOX[]={1};
+	// int NBOX[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,18,20};
+	int NBOX[]={1,2};
 
 	ofstream file_ba("block_analysis.dat");
 
@@ -460,6 +471,7 @@ int main(int argc, char* argv[])
 				find_nearest_coo( D1_loc, D2_loc, X_loc, Npoints, ncoords, periodicB); 
 
 				compute_d( D1_loc, D2_loc, dim, Npoints, frac, nbox, N_retained);
+				cout << dim << endl;
 				AVE_D[i]+=dim;
 				VAR_D[i]+=dim*dim;
 			}else
@@ -498,7 +510,7 @@ int main(int argc, char* argv[])
 		if(i==0 | i==1 | i==2 | i==3) 
 		{
 			dim_tot=dim;
-			cout << dim_tot << endl;
+			// cout << dim_tot << endl;
 		}
 	}
 
@@ -512,32 +524,32 @@ int main(int argc, char* argv[])
 	// block analysis plot
 
 
-	// ofstream file_gpl_ba("block_analysis.gpl");
+	ofstream file_gpl_ba("block_analysis.gpl");
 
-	// file_gpl_ba<< "set key top right box lw 1  lc rgb \"#7F7F7F\" font \",10\" spacing 10" <<endl;
-	// file_gpl_ba<< "set border 4095 lw 1 lc rgb \"#7F7F7F\" " <<endl;
-	// file_gpl_ba<< "set grid lw 1" <<endl;
-	// file_gpl_ba<< "set yrange["<<lower_bound<<":"<<upper_bound<<"]"<<endl; 
-	// file_gpl_ba<< "set title \"block analysis "<<file_in<<"\" font \",20\"" <<endl;
-	// file_gpl_ba<< "set xlabel \"N\" font \",15\"" <<endl;
-	// file_gpl_ba<< "set ylabel \"d\" font \",15\" rotate by 0" <<endl;
-	// file_gpl_ba<< "p \'block_analysis.dat\' u 1:2 w lp lw 2 lc rgb \"#DC143C\" not, '' u 1:2:3 w e lc rgb \"#DC143C\" not, "<< dim_tot<<" lw 2 lc rgb \"#00BFFF\" t \"  full dataset dimension\"" <<endl;
-	// system("gnuplot -persist block_analysis.gpl");
+	file_gpl_ba<< "set key top right box lw 1  lc rgb \"#7F7F7F\" font \",10\" spacing 10" <<endl;
+	file_gpl_ba<< "set border 4095 lw 1 lc rgb \"#7F7F7F\" " <<endl;
+	file_gpl_ba<< "set grid lw 1" <<endl;
+	file_gpl_ba<< "set yrange["<<lower_bound<<":"<<upper_bound<<"]"<<endl; 
+	file_gpl_ba<< "set title \"block analysis "<<file_in<<"\" font \",20\"" <<endl;
+	file_gpl_ba<< "set xlabel \"N\" font \",15\"" <<endl;
+	file_gpl_ba<< "set ylabel \"d\" font \",15\" rotate by 0" <<endl;
+	file_gpl_ba<< "p \'block_analysis.dat\' u 1:2 w lp lw 2 lc rgb \"#DC143C\" not, '' u 1:2:3 w e lc rgb \"#DC143C\" not, "<< dim_tot<<" lw 2 lc rgb \"#00BFFF\" t \"  full dataset dimension\"" <<endl;
+	system("gnuplot -persist block_analysis.gpl");
 
-	// file_gpl_ba.close();
+	file_gpl_ba.close();
 	
-	// // S-set plot
+	// S-set plot
 	
-	// ofstream file_gpl_fun("S_set.gpl");
- //    	file_gpl_fun<<fixed;
-	// file_gpl_fun.precision(2);
-	// file_gpl_fun<< "set key top left box lw 1  lc rgb \"#7F7F7F\" font \",10\" spacing 5" <<endl;
-	// file_gpl_fun<< "set border 4095 lw 1 lc rgb \"#7F7F7F\" " <<endl; 
-	// file_gpl_fun<< "set title \"S set "<<file_in<<"\" font \",20\"" <<endl;
-	// file_gpl_fun<< "p \'fun.dat\' lc rgb \"#7F7F7F\" pt 7 not, "<<"\"<head -"<<N_retained<<" 'fun.dat'\" lc rgb \"#DC143C\" pt 7 t \"S set\", "<<dim_tot<<"*x"<<" lw 2 lc rgb \"#00BFFF\" t \" "<<dim_tot<<"*x\"" <<endl;
-	// system("gnuplot -persist S_set.gpl");
+	ofstream file_gpl_fun("S_set.gpl");
+    	file_gpl_fun<<fixed;
+	file_gpl_fun.precision(2);
+	file_gpl_fun<< "set key top left box lw 1  lc rgb \"#7F7F7F\" font \",10\" spacing 5" <<endl;
+	file_gpl_fun<< "set border 4095 lw 1 lc rgb \"#7F7F7F\" " <<endl; 
+	file_gpl_fun<< "set title \"S set "<<file_in<<"\" font \",20\"" <<endl;
+	file_gpl_fun<< "p \'fun.dat\' lc rgb \"#7F7F7F\" pt 7 not, "<<"\"<head -"<<N_retained<<" 'fun.dat'\" lc rgb \"#DC143C\" pt 7 t \"S set\", "<<dim_tot<<"*x"<<" lw 2 lc rgb \"#00BFFF\" t \" "<<dim_tot<<"*x\"" <<endl;
+	system("gnuplot -persist S_set.gpl");
 
-	// file_gpl_fun.close();
+	file_gpl_fun.close();
 		
 	return 0;
 
--- two_nn.py
+++ two_nn.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def load_data(file_path):
 	"""
@@ -10,23 +10,150 @@ def load_data(file_path):
 	return data
 
 
+# def load_data_pd(file_path):
+
+
 def load_data_pkl(file_path):
 	with open(input_file, 'rb') as file:
-        return np.load(file_path)
+		return np.load(file_path)
 
-# def two_nn_id():
 
-def calc_2nn(data):
-	for i in data:
-		for j in data:
+def dist_mat(data, metric):
+	"""
+	Computes the distance matrix of a dataset given a specific metric function.
+	The diagonal is set to float("inf")
+	:param data: dataset as a 2d numpy array.
+	:param metric: function that defines the metric.
+	"""
+	N_samples = data.shape[0]
+	N_coord = data.shape[1]
+	dist_mat = np.zeros((N_samples, N_samples))
+	for i in range(N_samples):
+		dist_mat[i,i] = float("inf")
+		for j in range(i+1,N_samples):
+			dist_mat[i,j] = metric(data[i,:], data[j,:])
+			dist_mat[j,i] = dist_mat[i,j]
+	return dist_mat
 
-# def compute_id():
 
-# def eucl_dist(x,y):
-# 	return x**2 - y**2
+def arr_two_nn(array):
+	first = float("inf")
+	second = float("inf")
+	for val in array:
+		if val < first:
+			second = first
+			first = val
+		elif val < second and second != first:
+			second = val
+	return np.sqrt(first), np.sqrt(second)
 
+
+
+def two_nn_id(data, frac = .9):
+	"""
+	Computes the intrinsic dimension of a given dataset using the TWO-NN algorithm.
+	:param data: dataset as a 2d numpy array.
+	:param frac: fraction of points used to calculate the slope of the linear regression. \
+	Not to be confused with the fraction of data used in the block analysis.
+	"""
+	N_samples = data.shape[0]
+	N_coord = data.shape[1]
+	# print(f"N_samples: {N_samples}")
+	N_frac = int(N_samples*frac)
+	print(f'N_frac: {N_frac}')
+	if N_frac == 0:
+		N_frac = 1
+	assert N_frac <= N_samples and N_frac > 0, 'Frac must be a real number between 0 and 1.'
+
+	d_mat = dist_mat(data, eucl_dist2)
+
+	first_arr = []
+	second_arr = []
+	for row in d_mat:
+		first, second = arr_two_nn(row)
+		first_arr.append(first)
+		second_arr.append(second)
+
+	mu = np.array(second_arr)/np.array(first_arr)
+
+	# mu is sorted
+	mu, mu_cs = cum_sum(mu)
+	# mu = np.sort(mu)
+
+	# linearize the equation F(mu) = (1-mu**(-d))
+	y = - np.log(1-mu_cs)[:N_frac]
+	x = np.log(mu)[:N_frac]
+	print(f'len x: {len(x)}')
+
+	# Linear regression with intercept = 0
+	dim = np.sum(x*y) / np.sum(x*x)
+	# alternative using numpy
+	# x = x[:,np.newaxis]
+	# dim, _, _, _ = np.linalg.lstsq(x, y, rcond=None)
+
+	print(dim)
+	return dim, x, y
+
+
+def eucl_dist2(x,y):
+	""" Computes the euclidean distance between two points"""
+	return sum((x - y)**2)
+
+
+def cum_sum(x):
+	x_sort = np.sort(x)
+	x_len = len(x)
+	# calculate the proportional values of samples
+	# all of the above are unbiased estimations of the CDF
+	# p = 1. * np.arange(len(data)) / (len(data) - 1)
+	# p = (arange(len(x))+0.5)/len(x)
+	# p = np.linspace(0, 1, len(data), endpoint=False)
+	p = (np.linspace(0, x_len - 1 , x_len)) / x_len
+	return x_sort, p
+
+
+def two_nn_block_analysis(data, frac, num_blocks = 2):
+	"""
+	Separate the
+	:param num_blocks: number of different block sizes to do the scale analysis. \
+	If num_blocks = 5, you are scaling down the dataset up to 20% of its original size.
+	"""
+	N_samples = data.shape[0]
+	blocks_dim_avg = []
+	blocks_dim_std = []
+	blocks_dim_lst = np.arange(1, num_blocks+1)
+	# nblock_l = np.linspace(1, num_blocks, num_blocks)
+
+	for nblock in range(1, num_blocks+1):
+		# index = np.arange(N_samples, dtype = int)
+		# np.random.shuffle( index )
+		rem = N_samples % nblock
+		int_div = int(N_samples / nblock)
+
+		dim = []
+		for j in range(nblock):
+	 		# Divide data points in such that localy: rows = rows_loc ; cols = dim
+			size = int_div + 1 - int((nblock - rem + j)/nblock) # it distributes the remainder in a round robin way starting from j = 0. 
+			# Not the easiest way of doing it, but I wanted to try without the if().	 		
+	 		# Define global row index i
+			start = j * (int_div + 1) - int((nblock - rem + j)/nblock) * (j-rem)
+
+			dim.append(two_nn_id(data[index[int(start):int(start+size)].astype(int)], frac))
+		blocks_dim_avg.append(np.mean(dim))
+		blocks_dim_std.append(np.std(dim))
+	return blocks_dim_avg, blocks_dim_std, N_samples/blocks_dim_lst
 
 
 
 if __name__ == "__main__":
 	data = load_data('./datasets/cauchy20')
+	index = load_data('index.dat')
+	# dim, mu, mu_cs = two_nn_id(data, 0.9)
+	blocks_dim, _, blocks_size = two_nn_block_analysis(data, .9)
+	# print(dim)
+	# plt.plot(mu,mu_cs)
+	# plt.show()
+	# print(blocks_dim)
+	# print(blocks_size)
+	# plt.plot(blocks_size, blocks_dim)
+	# plt.show()
